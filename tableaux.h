ddae3ff221bcb69689988c83a102352b74980d91
ValentinPEIRETTI <91471528+ValentinPEIRETTI@users.noreply.github.com>
2021-10-12 01:52:42 +0200

    Create tableaux.h

---
+++ tableaux.h
@@ -0,0 +1,9 @@
+#ifndef TP5_TABLEAUX_H
+#define TP5_TABLEAUX_H
+#define NB_LIGNES 3
+#define NB_COLONNES 4
+
+void afficherTableau2D(int tableau[NB_LIGNES][NB_COLONNES]);
+void afficherTableau1D(int tableau[NB_LIGNES][NB_COLONNES]);
+
+#endif //TP5_TABLEAUX_H
